2bc54778e0661f09af509f37d811a04900febeec
Sam Kuhbander <kuhbansc@miamioh.edu>
2023-03-17 21:11:32 -0400

    added images for projects

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,10 +11,20 @@ const Contact = () => {
       </Row>
       <Row>
         <Col>
-          <p>
-            If you'd like to get in touch with me, please feel free to email me at:{' '}
-            <a href="kuhbandersam@gmail.com">kuhbandersam@gmail</a>
-          </p>
+          <div className="mt-4">
+            <p>
+              If you'd like to get in touch with me, please feel free to email me at:
+            </p>
+            <p>
+              <a
+                href="mailto:kuhbandersam@gmail.com"
+                className="font-weight-bold text-primary"
+                style={{ textDecoration: 'none', fontSize: '1.25rem' }}
+              >
+                kuhbandersam@gmail.com
+              </a>
+            </p>
+          </div>
         </Col>
       </Row>
     </Container>

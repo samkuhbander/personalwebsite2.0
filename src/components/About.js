76880dedd28eba3c82c4f9a3f772d0a277302cca
samkuhbander <55243087+samkuhbander@users.noreply.github.com>
2023-07-04 21:08:29 -0400

    Changed bio and made photo smaller

--- src/components/About.js
+++ src/components/About.js
@@ -11,12 +11,12 @@ const About = () => {
       </Row>
       <Row>
         <Col md={3}>
-            <img src="Headshot.png" alt="Sam Kuhbander" className="img-fluid" height="300" width="300" />
+          <img src="Headshot.jpeg" alt="Sam Kuhbander" className="img-fluid" height="300" width="300" />
         </Col>
         <Col md={9}>
           <p>
-            Hello! My name is Sam Kuhbander, and I'm a software engineer graduating from Miami University. I have a passion for
-            creating innovative software solutions and enjoy working with modern web technologies.
+            Hello! My name is Sam Kuhbander, and I'm a software engineer with a degree from Miami University.
+            I have a passion for creating innovative software solutions and enjoy working with modern web technologies. I'm currently pursuing a Master's degree in Computer Science at the University of Illinois Urbana-Champaign.
           </p>
           <p>
             I have experience in full-stack development, using technologies such as JavaScript, React, Node.js, Java, and Python. I'm

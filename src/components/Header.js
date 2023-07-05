ae93230216db6c7ce9fbeb6375c8a4ad70308ed8
samkuhbander <55243087+samkuhbander@users.noreply.github.com>
2023-07-04 21:21:18 -0400

    Changed navigation on page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 import { FaLinkedin, FaGithub, FaTwitter } from 'react-icons/fa';
+import { Link as ScrollLink } from 'react-scroll';
 
 const Header = () => {
   const [expanded, setExpanded] = useState(false);
@@ -11,14 +12,14 @@ const Header = () => {
 
   return (
     <Navbar bg="light" expand="lg" sticky="top" expanded={expanded} className="shadow-sm p-3 bg-white rounded">
-      <Navbar.Brand href="#about" onClick={closeNavbar}>Samuel Kuhbander</Navbar.Brand>
+      <Navbar.Brand as={ScrollLink} to="about" onClick={closeNavbar} spy={true} smooth={true} duration={100}>Samuel Kuhbander</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setExpanded(!expanded)} />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
-          <Nav.Link href="#about" onClick={closeNavbar}>About Me</Nav.Link>
-          <Nav.Link href="#experience" onClick={closeNavbar}>Experience</Nav.Link>
-          <Nav.Link href="#projects" onClick={closeNavbar}>Projects</Nav.Link>
-          <Nav.Link href="#contact" onClick={closeNavbar}>Contact</Nav.Link>
+          <Nav.Link as={ScrollLink} to="about" onClick={closeNavbar} spy={true} smooth={true} duration={100}>About Me</Nav.Link>
+          <Nav.Link as={ScrollLink} to="experience" onClick={closeNavbar} spy={true} smooth={true} duration={100}>Experience</Nav.Link>
+          <Nav.Link as={ScrollLink} to="projects" onClick={closeNavbar} spy={true} smooth={true} duration={100}>Projects</Nav.Link>
+          <Nav.Link as={ScrollLink} to="contact" onClick={closeNavbar} spy={true} smooth={true} duration={100}>Contact</Nav.Link>
           <Nav.Link href="https://www.linkedin.com/in/samuel-kuhbander/" target="_blank" rel="noopener noreferrer">
             <FaLinkedin size={25} />
           </Nav.Link>
